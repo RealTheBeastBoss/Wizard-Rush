fdbee756b3eedd42c05f181e48f333af6c0c7314
RealTheBeastBoss <98669913+RealTheBeastBoss@users.noreply.github.com>
2024-05-09 19:10:04 +0100

    Initial commit

---
+++ scenes/menus/settings_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var fullscreen = $Buttons/Fullscreen
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menus/start_menu.tscn")
+
+func _on_fullscreen_pressed():
+	if fullscreen.button_pressed:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
---
+++ scripts/start_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/menus/settings_menu.tscn")
